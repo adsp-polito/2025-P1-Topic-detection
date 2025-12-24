3c5120206bf4163c1b63562c372da1d2663453a3
Sonia Foco <sonia.foco02@gmail.com>
2025-12-24 15:49:00 +0100

    added testing of different BERTopic architectures

--- topic_modeler.py
+++ topic_modeler.py
@@ -6,6 +6,8 @@ from nltk.corpus import stopwords
 from sentence_transformers import SentenceTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from umap import UMAP
+from sklearn.cluster import KMeans, SpectralClustering
+from sklearn.decomposition import KernelPCA
 
 from config import cfg
 from logger import WandBLogger
@@ -15,13 +17,32 @@ class TopicModeler:
     """
     BERTopic wrapper with SOTA Italian configurations and WandB logging.
     """
+    ARCHITECTURES = {
+    #Default
+    "umap_hdbscan": {
+        "reduction": "umap",
+        "clustering": "hdbscan"
+        },
+    "kernelpca_spectral": {
+        "reduction": "kernel_pca",
+        "clustering": "spectral"
+        },
+    "kernelpca_kmeans": {
+        "reduction": "kernel_pca",
+        "clustering": "kmeans"
+        },
+    "umap_spectral": {
+        "reduction": "umap",
+        "clustering": "spectral"
+        }
+    }
 
     def __init__(self):
         self.config = cfg.get("topic_modeling")
         self.project_name = cfg.get("project.name")
         self.embedding_model = None
         self.topic_model = None
-
+    
         # Setup Italian Stopwords
         nltk.download("stopwords", quiet=True)
         self.stop_words = stopwords.words("italian")
@@ -29,17 +50,71 @@ class TopicModeler:
         extras = self.config.get("extra_stopwords", [])
         self.stop_words.extend(extras)
 
-    def run(self, docs: list, run_name: str = "bertopic_run"):
+    def get_reduction(self, name):
+        if name == "umap":
+            ucfg = self.config.get("umap")
+            return UMAP(
+                n_neighbors=ucfg["n_neighbors"],
+                n_components=ucfg["n_components"],
+                min_dist=ucfg["min_dist"],
+                metric=ucfg["metric"],
+                random_state=cfg.get("project.seed", 42),
+            )
+
+        if name == "kernel_pca":
+            kcfg = self.config.get("kernel_pca")
+            return KernelPCA(
+                n_components=kcfg["n_components"],
+                kernel=kcfg.get("kernel", "rbf"),
+                random_state=cfg.get("project.seed", 42),
+            )
+
+        raise ValueError(f"Unknown reduction: {name}")
+    
+    def get_clustering(self, name):
+        if name == "hdbscan":
+            hcfg = self.config.get("hdbscan")
+            return HDBSCAN(
+                min_cluster_size=hcfg["min_cluster_size"],
+                metric="euclidean",
+                cluster_selection_method=hcfg["cluster_selection_method"],
+                prediction_data=True,
+            )
+
+        if name == "kmeans":
+            kcfg = self.config.get("kmeans")
+            return KMeans(
+                n_clusters=kcfg["n_clusters"],
+                random_state=cfg.get("project.seed", 42),
+                n_init="auto",
+            )
+
+        if name == "spectral":
+            scfg = self.config.get("spectral")
+            return SpectralClustering(
+                n_clusters=scfg["n_clusters"],
+                affinity="nearest_neighbors",
+                random_state=cfg.get("project.seed", 42),
+            )
+
+        raise ValueError(f"Unknown clustering: {name}")
+
+
+    def run(self, docs: list, run_name: str = "bertopic_run", architecture_name : str = "umap_hdbscan", logger=None):
         """
         Executes the topic modeling pipeline.
         """
         model_type = self.config.get("model_choice")
         print(f"--> [BERTopic] Starting run: {run_name} using [{model_type}] config")
 
-        # 1. Initialize WandB
-        logger = WandBLogger()
+        architecture = self.ARCHITECTURES[architecture_name]
+        print(f"    Running architecture: {architecture}")
 
-        # 2. Select and Store Embedding Model
+        # Wandb
+        if cfg.get("project.wandb_logging") and logger is None:
+            print("    Warning: WandBLogger must be passed from main()")
+
+        # 1. Select and Store Embedding Model
         # We store it in 'self' to access it later for evaluation
         model_name = self.config.get("embedding_model")
         print(f"    Loading embedding model: {model_name}...")
@@ -49,35 +124,24 @@ class TopicModeler:
         print("    Encoding embeddings...")
         embeddings = self.embedding_model.encode(docs, show_progress_bar=True)
 
-        # 3. Dimensionality Reduction (UMAP)
-        umap_conf = self.config.get("umap")
-        umap_model = UMAP(
-            n_neighbors=umap_conf["n_neighbors"],
-            n_components=umap_conf["n_components"],
-            min_dist=umap_conf["min_dist"],
-            metric=umap_conf["metric"],
-            random_state=cfg.get("project.seed", 42),
-        )
+        # 2. Dimensionality Reduction 
+        print(f"    Loading dimensionality reduction model ({architecture['reduction']})...")
+        dim_red_model = self.get_reduction(architecture['reduction'])  
 
-        # 4. Clustering (HDBSCAN)
-        hdb_conf = self.config.get("hdbscan")
-        hdbscan_model = HDBSCAN(
-            min_cluster_size=hdb_conf["min_cluster_size"],
-            metric="euclidean",
-            cluster_selection_method=hdb_conf["cluster_selection_method"],
-            prediction_data=True,
-        )
+        # 3. Clustering 
+        print(f"    Loading clustering model ({architecture['clustering']})...")
+        cluster_model = self.get_clustering(architecture['clustering'])
 
-        # 5. Vectorizer (Topic Representation) min_df=2 ensures words appear in at least 2 topics (or docs), preventing the crash if topics are few.
+        # 4. Vectorizer (Topic Representation) min_df=2 ensures words appear in at least 2 topics (or docs), preventing the crash if topics are few.
         vectorizer_model = CountVectorizer(
             stop_words=self.stop_words, min_df=self.config.get("min_df", 2)
         )
 
-        # 6. Initialize and Fit BERTopic
+        # 5. Initialize and Fit BERTopic
         self.topic_model = BERTopic(
             embedding_model=self.embedding_model,
-            umap_model=umap_model,
-            hdbscan_model=hdbscan_model,
+            umap_model=dim_red_model,
+            hdbscan_model=cluster_model,
             vectorizer_model=vectorizer_model,
             language=self.config.get("language", "multilingual"),
             calculate_probabilities=True,
@@ -87,41 +151,41 @@ class TopicModeler:
         print("--> [BERTopic] Fitting model...")
         topics, probs = self.topic_model.fit_transform(docs, embeddings=embeddings)
 
-        # 7. Logging to WandB
+        # 6. Logging to WandB
         freq = self.topic_model.get_topic_info()
         n_topics = len(freq) - 1
         print(f"--> [BERTopic] Generated {n_topics} topics.")
 
-        logger.log_metrics({"n_topics": n_topics, "n_docs": len(docs)})
+        if logger is not None:
+            logger.log_metrics({"n_topics": n_topics, "n_docs": len(docs)})
+            # Log Topic Info Table
+            wandb.log({"topic_info": wandb.Table(dataframe=freq)})
 
-        # Log Topic Info Table
-        logger.log_plot("topic_info", wandb.Table(dataframe=freq), plot_type="table")
+            # --- ADVANCED PLOTS ---
+            try:
+                # A. Intertopic Distance
+                logger.log_plot("plot_intertopic", self.topic_model.visualize_topics())
 
-        # --- ADVANCED PLOTS ---
-        try:
-            # A. Intertopic Distance
-            logger.log_plot("plot_intertopic", self.topic_model.visualize_topics())
-
-            # B. Bar Chart (Top 15)
-            logger.log_plot(
-                "plot_barchart", self.topic_model.visualize_barchart(top_n_topics=15)
-            )
-
-            # C. Hierarchy (Tree) - Addresses Task 3b
-            # Limit to top 50 topics to keep it readable
-            fig_hierarchy = self.topic_model.visualize_hierarchy(top_n_topics=50)
-            logger.log_plot("plot_hierarchy", fig_hierarchy)
-
-            # D. Similarity Heatmap - Addresses Topic Separation
-            # We filter out Topic -1 (Noise) for the heatmap
-            if n_topics > 1:
-                fig_heatmap = self.topic_model.visualize_heatmap(
-                    n_clusters=n_topics - 1
+                # B. Bar Chart (Top 15)
+                logger.log_plot(
+                    "plot_barchart", self.topic_model.visualize_barchart(top_n_topics=15)
                 )
-                logger.log_plot("plot_heatmap", fig_heatmap)
 
-        except Exception as e:
-            print(f"--> [Warning] Could not log plots: {e}")
+                # C. Hierarchy (Tree) - Addresses Task 3b
+                # Limit to top 50 topics to keep it readable
+                fig_hierarchy = self.topic_model.visualize_hierarchy(top_n_topics=50)
+                logger.log_plot("plot_hierarchy", fig_hierarchy)
+
+                # D. Similarity Heatmap - Addresses Topic Separation
+                # We filter out Topic -1 (Noise) for the heatmap
+                if n_topics > 1:
+                    fig_heatmap = self.topic_model.visualize_heatmap(
+                        n_clusters=n_topics - 1
+                    )
+                    logger.log_plot("plot_heatmap", fig_heatmap)
+
+            except Exception as e:
+                print(f"--> [Warning] Could not log plots: {e}")
 
         return self.topic_model, topics, probs
 
@@ -133,3 +197,4 @@ class TopicModeler:
             print(f"--> [BERTopic] Model saved locally to {path}")
         else:
             print("--> [Error] No model to save. Run .run() first.")
+
